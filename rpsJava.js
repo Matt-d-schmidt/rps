37c8bbfd42dca14d0481717956135240da8bb762
Matt-d-schmidt <12345689+05schmidtm18@gmail.com.github.com>
2025-08-01 22:16:34 -0400

    adding rps.html to branch

--- rpsJava.js
+++ rpsJava.js
@@ -1,16 +1,16 @@
-function getRandomInt(max){
+function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
 function getComputerChoice() {
     let ran = getRandomInt(3);
-    switch(ran) {
+    switch (ran) {
         case 0:
             return "Rock";
         case 1:
             return "Paper";
-        case 2: 
-            return "Scissors"    
+        case 2:
+            return "Scissors"
     }
 }
 
@@ -19,47 +19,49 @@ function getHumanChoice() {
     return choice;
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    
-    function playRound(computerChoice, humanChoice) {
-       let hChoice = humanChoice.toLowerCase();
-       let cChoice = computerChoice
+let humanScore = 0;
+let computerScore = 0;
 
-        if(hChoice === "rock" && cChoice === "Scissors"){
-            console.log( "You win! " + hChoice + " beats " + cChoice);
-            humanScore = ++humanScore
-        } else if (hChoice === "paper" && cChoice === "Rock"){
-            console.log( "You win! " + hChoice + " beats " + cChoice);
-            humanScore = ++humanScore
-        } else if (hChoice === "scissors" && cChoice === "Paper"){
-            console.log("You win! " + hChoice + " beats " + cChoice);
-            humanScore = ++humanScore
-        } else if (hChoice === cChoice) {
-            console.log( "It's a tie! you both picked " + hChoice);
-            return;
-        } else {
-            console.log( "You lose! " + hChoice + " beats " + cChoice);
-            computerScore = ++computerScore
-        }
-    }
+function playRound(computerChoice, humanChoice) {
+    let hChoice = humanChoice.toLowerCase();
+    let cChoice = computerChoice
 
-    for (let i = 0; i < 5; i++){
-        let computerChoise = getComputerChoice();
-        let humanChoice = getHumanChoice();
-
-        playRound(computerChoise, humanChoice);
-    }
-    console.log('Final Score - YOU: ' + humanScore + ', COMPUTER: ' + computerScore);
-
-    if (humanScore > computerScore) {
-        console.log("You won the game!");
-    } else if (computerScore > humanScore) {
-        console.log("You lost the game.");
+    if (hChoice === "rock" && cChoice === "Scissors") {
+        console.log("You win! " + hChoice + " beats " + cChoice);
+        humanScore = ++humanScore
+    } else if (hChoice === "paper" && cChoice === "Rock") {
+        console.log("You win! " + hChoice + " beats " + cChoice);
+        humanScore = ++humanScore
+    } else if (hChoice === "scissors" && cChoice === "Paper") {
+        console.log("You win! " + hChoice + " beats " + cChoice);
+        humanScore = ++humanScore
+    } else if (hChoice === cChoice) {
+        console.log("It's a tie! you both picked " + hChoice);
+        return;
     } else {
-        console.log("The game is a tie.");
+        console.log("You lose! " + hChoice + " beats " + cChoice);
+        computerScore = ++computerScore
     }
 }
 
-playGame();
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const computerChoice = getComputerChoice();
+        const humanChoice = button.textContent;
+        playRound(computerChoice, humanChoice);
+        results.textContent = `You chose ${humanChoice}, Computer chose ${computerChoice}. 
+                                 Score - YOU: ${humanScore}, COMPUTER: ${computerScore}`;
+        if (humanScore === 5) {
+            results.textContent += " You win the game!";
+            humanScore = 0;
+            computerScore = 0;
+        } else if (computerScore === 5) {
+            results.textContent += " Computer wins the game!";
+            humanScore = 0;
+            computerScore = 0;
+        } else {
+            results.textContent += " Keep playing!";
+        }
+    });
+});
